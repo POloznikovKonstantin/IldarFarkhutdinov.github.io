ce98241e53c94f040c9d2bfeb185a09d8aafaf6d
s1ghe <golikovefim65@gmail.com>
2023-08-15 10:56:12 +0300

    MainLink Comp new, edd locales

--- lend/src/App.tsx
+++ lend/src/App.tsx
@@ -1,28 +1,19 @@
 import React, {FC, useEffect} from 'react';
 import {useTranslation} from 'react-i18next';
-import { HeaderSection } from './components/UI/MainSections/HeaderSetion/HeaderSection';
-import { SubTitleSection } from './components/UI/MainSections/SubTitleSection/SubTitleSection';
+import {MainLink} from "./components/UI/Links/MainLinks/MainLink";
 
 export const App:FC = () => {
   const {i18n} = useTranslation();
 
   useEffect(() => {
-    i18n.changeLanguage('EN')
+    i18n.changeLanguage('RU')
   }, [i18n])
-
 
   return (
     <main className='container'>
-      <HeaderSection 
-        header='Components.UI.MainSection.HeaderSection.ProfessionalProfile'
-        subTitle='Components.UI.MainSection.HeaderSection.ProfessionalProfile'
-      />
-
-      <SubTitleSection 
-        header='Components.UI.MainSection.HeaderSection.ProfessionalProfile'
-        subTitle='Components.UI.MainSection.HeaderSection.ProfessionalProfile'
-      />
-
+      <MainLink href={'/#'}>
+          Components.UI.Buttons.MainButtons.OpenGitHub
+      </MainLink>
     </main>
   )
 }
--- lend/src/components/UI/MainSections/HeaderSetion/HeaderSection.tsx
+++ lend/src/components/UI/MainSections/HeaderSetion/HeaderSection.tsx
@@ -1,14 +1,15 @@
 import React, {FC} from 'react';
 import {useTranslation} from 'react-i18next';
 import './HeaderSection.css';
+import {WorkSvg} from "../../../../utils/WorkSvg";
 
-interface HeaderSectionProps{
+interface IHeaderSectionProps{
     header: string,
     subTitle?:string,
     className?:string
 }
 
-export const HeaderSection:FC<HeaderSectionProps> = ({header, subTitle, className=''}) => {
+export const HeaderSection:FC<IHeaderSectionProps> = ({header, subTitle, className=''}) => {
     const {t} = useTranslation();
 
     return (
@@ -16,13 +17,11 @@ export const HeaderSection:FC<HeaderSectionProps> = ({header, subTitle, classNam
             <div className='header-section__content'>
                 <h2 className='header-text'>
                     <span className='lattice-span'>#</span>
-                    <span>{t(header)}</span>
+                    {t(header)}
                 </h2>
 
                 <div className='header-section__svg'>
-                    <svg width="150" height="2" viewBox="0 0 228 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="228" height="2" fill="#F29C9B"/>
-                    </svg>
+                    <WorkSvg nameSvg={'RecTangle'}/>
                 </div>
             </div>
 
