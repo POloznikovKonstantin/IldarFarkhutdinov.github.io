bdaedbd1d3043037072ea9961119609058b29329
Efim <94549535+S1GHE@users.noreply.github.com>
2023-08-21 01:06:44 +0300

    Revert "Создание маленьких карточек в блоке professinal profile"

--- lend/src/App.tsx
+++ lend/src/App.tsx
@@ -1,12 +1,6 @@
-/* eslint-disable react/jsx-no-undef */
 import React, {FC, useEffect} from 'react';
 import {useTranslation} from 'react-i18next';
-import { CardBg } from './assets';
-import { CardProject } from './components/UI/Card/CardProject/CardProject';
-import { CardProfessionalProfile } from './components/UI/Card/CardProfesionalProfile/CardProfessionalProfile';
-import { CardEducation } from './components/UI/Card/CardEducation/CardEducation';
-import { HWULogo } from './assets';
-
+import AppTest from "./AppTest";
 
 export const App: FC = () => {
     const {i18n} = useTranslation();
@@ -15,15 +9,9 @@ export const App: FC = () => {
         i18n.changeLanguage('EN')
     }, [i18n])
 
-  return (
-    <main className='container'>
-      {/* <MainLink href={'/#'}>
-          Components.UI.Buttons.MainButtons.OpenGitHub
-      </MainLink>*/}
-      <CardProject NameHeader='Components.UI.Card.CardProject.ITProjects.NameHeader' Stack='Components.UI.Card.CardProject.ITProjects.Stack' Description='Components.UI.Card.CardProject.ITProjects.Description' Link='/#' ImgLink={CardBg}/>
-      <CardProfessionalProfile CardHeader='Components.UI.Card.CardProfessionProfile.Profile.CardHeader' CardSubHeader='Components.UI.Card.CardProfessionProfile.Profile.CardSubHeader' CardDescription='Components.UI.Card.CardProfessionProfile.Profile.CardDescription'/>
-      <CardEducation CardEducationHeader='Components.UI.Card.CardEducation.Education.CardEducationHeader' CardEducationYears='Components.UI.Card.CardEducation.Education.CardEducationYears' CardEducationProfession='Components.UI.Card.CardEducation.Education.CardEducationProfession' imgLogo={HWULogo}/>
-    </main>
-  )
-
+    return (
+        <main className='container'>
+            <AppTest/>
+        </main>
+    )
 }
--- lend/src/assets/index.ts
+++
@@ -1,7 +0,0 @@
-import CardBg from './img/card-bg.jpg';
-import HWULogo from './svg/HWULogo.svg'
-
-export {
-    CardBg, HWULogo,
-}
-
