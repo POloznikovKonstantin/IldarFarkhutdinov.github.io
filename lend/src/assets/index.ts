d125fdf13f91c5f7af2ef2504c28c09c142aeb7c
POloznikovKonstantin <Kost.Poloznikov.ya@gmail.com>
2023-08-23 08:33:02 +0300

    edd logo

--- lend/src/assets/index.ts
+++ lend/src/assets/index.ts
@@ -1,7 +1,11 @@
 import CardBg from './img/card-bg.jpg';
-import HWULogo from './svg/HWULogo.svg'
+import HWULogo from './svg/HWULogo.svg';
+import TPYLogo from './svg/TPY_Logo.svg';
+import YandexLogo from './img/Yandex.png';
+import School21Sber from './img/21sber.png';
+import BMSTULogo from './img/BMSTU_white.png';
 
 export {
-    CardBg, HWULogo,
+    CardBg, HWULogo, TPYLogo, YandexLogo, School21Sber, BMSTULogo
 }
 
